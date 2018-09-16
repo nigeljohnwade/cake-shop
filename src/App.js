268d14b63d84960cfe7cfb6b6efb9eb2e9ae31b7
nigeljohnwade <nigeljohnwade@gmail.com>
2018-09-16 17:59:09 +0100

    Adds router and implements home route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
 
 class App extends Component {
     render() {
         return (
-            <div>
+            <Fragment>
                 <header className="header">
                       <h1 className="title">Welcome to Cake</h1>
                 </header>
                 <main role="main">
+                    <Router>
+                        <Switch>
+                            <Route path="/" component={Home}/>
+                        </Switch>
+                    </Router>
                 </main>
-            </div>
+            </Fragment>
     );
   }
 }
