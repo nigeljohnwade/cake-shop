2ca43cec929c57d9c314f3b5891419e009b3c7ee
nigeljohnwade <nigeljohnwade@gmail.com>
2018-09-16 13:44:54 +0100

    Initialise react app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <header className="header">
+                      <h1 className="title">Welcome to Cake</h1>
+                </header>
+                <main role="main">
+                </main>
+            </div>
+    );
+  }
+}
+
+export default App;
