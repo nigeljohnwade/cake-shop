58bb379f7c8b5b2f1d69d58a83f574bb1d3b20b5
nigeljohnwade <nigeljohnwade@gmail.com>
2018-09-17 10:58:12 +0100

    Adds detail route and scroll to top component

--- src/App.js
+++ src/App.js
@@ -3,34 +3,42 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import New from './components/New';
+import Detail from './components/Detail';
 import Header from "./components/Header";
+import ScrollTop from './components/ScrollTop';
 
 class App extends Component {
     render() {
         return (
             <Router>
-                <Fragment>
-                    <Route
-                        path="/" 
-                        component={Header}
-                    />
-                    <main role="main">
-                        <Switch>
-                            <Route
-                                path="/"
-                                exact={true}
-                                component={Home}
-                            />
-                            <Route 
-                                path="/new" 
-                                component={New}
-                            />
-                        </Switch>
-                    </main>
-                </Fragment>
+                <ScrollTop>
+                    <Fragment>
+                        <Route
+                            path="/" 
+                            component={Header}
+                        />
+                        <main role="main">
+                            <Switch>
+                                <Route
+                                    path="/"
+                                    exact={true}
+                                    component={Home}
+                                />
+                                <Route 
+                                    path="/new" 
+                                    component={New}
+                                />
+                                <Route
+                                    path="/cake/:id"
+                                    component={Detail}
+                                />
+                            </Switch>
+                        </main>
+                    </Fragment>
+                </ScrollTop>
             </Router>
-    );
-  }
+        );
+    }
 }
 
 export default App;
